70ef581e9d9a101804b550cc0969a7717a447715
Agus Priyatin <aguspriyatin80@gmail.com>
2022-03-09 23:42:25 +0700

    unit test - getAllBookRecords

---
+++ src/test/java/BookControllerTest.java
@@ -0,0 +1,67 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.rest.app.Book;
+import com.rest.app.BookController;
+import com.rest.app.BookRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookControllerTest {
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectWriter objectWriter = objectMapper.writer();
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookController bookController;
+
+    Book RECORD_1 = new Book(1L, "Atomic Habits", "How to build better habits", 5);
+    Book RECORD_2 = new Book(2L, "Thinking Fast and Slow", "How to create good mental", 4);
+    Book RECORD_3 = new Book(3L, "Grokking Algorithms", "Learn algorithm the fun way", 4);
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+    }
+
+    @Test
+    public void getAllRecords_success() throws Exception{
+        List<Book> records = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2, RECORD_3));
+        Mockito.when(bookRepository.findAll()).thenReturn(records);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/book")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[2].name").value("Grokking Algorithms"))
+                .andExpect(jsonPath("$[1].name").value("Thinking Fast and Slow"));
+
+
+    }
+}
